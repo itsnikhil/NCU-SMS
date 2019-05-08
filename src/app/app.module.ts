867c1de10bfa1996db34de70a13366cc500b9454
nishantkhatri29 <nishantkhatri29@gmail.com>
2019-05-08 14:24:29 +0530

    Nishant day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { from } from 'rxjs';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CartSummaryComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: "admin",component:AdminComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
