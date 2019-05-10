978740310c4a32a2ddbd6d74b6d57557c5439212
itsnikhil <taneja.nikhil03@gmail.com>
2019-05-10 11:08:03 +0530

    minor changes

--- src/app/model/product.repository.ts
+++ src/app/model/product.repository.ts
@@ -3,23 +3,23 @@ import { Product } from "./product.model";
 import { StaticDataSource } from "./static.datasource";
 @Injectable()
 export class ProductRepository {
- private products: Product[] = [];
- private categories: string[] = [];
- constructor(private dataSource: StaticDataSource) {
- dataSource.getProducts().subscribe(data => {
- this.products = data;
- this.categories = data.map(p => p.category)
- .filter((c, index, array) => array.indexOf(c) == index).sort();
- });
- }
- getProducts(category: string = null): Product[] {
- return this.products
- .filter(p => category == null || category == p.category).sort((a, b) => (a.quantity < b.quantity) ? 1 : -1);
- }
- getProduct(id: number): Product {
- return this.products.find(p => p.id == id);
- }
- getCategories(): string[] {
- return this.categories;
- }
+    private products: Product[] = [];
+    private categories: string[] = [];
+    constructor(private dataSource: StaticDataSource) {
+        dataSource.getProducts().subscribe(data => {
+            this.products = data;
+            this.categories = data.map(p => p.category)
+                .filter((c, index, array) => array.indexOf(c) == index).sort();
+        });
+    }
+    getProducts(category: string = null): Product[] {
+        return this.products
+            .filter(p => category == null || category == p.category).sort((a, b) => (a.quantity < b.quantity) ? 1 : -1);
+    }
+    getProduct(id: number): Product {
+        return this.products.find(p => p.id == id);
+    }
+    getCategories(): string[] {
+        return this.categories;
+    }
 }
