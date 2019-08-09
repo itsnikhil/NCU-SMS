340bc0495dcffaed6d6acf8a6786bf2f44a76e6b
itsnikhil <taneja.nikhil03@gmail.com>
2019-08-10 01:48:16 +0530

    fiddling around

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('data.js');
-const middlewares = jsonServer.router('authMiddleware.js');
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3500;
 
 server.use(middlewares);
