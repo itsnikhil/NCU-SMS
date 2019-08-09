86712476e467b8270f547938819a2088c67a1b1e
itsnikhil <taneja.nikhil03@gmail.com>
2019-08-10 02:02:11 +0530

    json server not working

--- server.js
+++ server.js
@@ -4,17 +4,6 @@ const path = require('path');
 
 const app = express();
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3500;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
-
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/NCU-SMS'));
 
